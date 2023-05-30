0055fcbf5c780438b32790c24fcacbb4c74aba96
angfigs <angfigs1475@gmail.com>
2023-05-29 21:27:25 -0700

    Added previous flashcard button.

--- script.js
+++ script.js
@@ -26,8 +26,6 @@ const alphabet_object = {
     Y: '',
     Z: ''
 };
-console.log(Object.keys(alphabet_object).length)
-
 
 // function create_alphabet() {
 //     for (let i = 65; i <= 90; i++) {
@@ -64,66 +62,82 @@ function generate_alphabet() {
     return alphabet[alphabet_index]    
 }
 
-//store the current letter in a variable and return it when the previous card button is pressed 
-
-// function previous_card() {
-
-// }
-
-
-
-const generate_letter = document.getElementById('flashcard_alphabet'); 
-const generate_button = document.getElementById('generate_button');
-const generate_alphabet_button = document.getElementById('generate_alphabet');
-const letter_element = generate_letter.querySelector('.updated_letter');
-
-
-// function hello() {
-//     if (letter_element.textContent === Object.keys(imgs).toString()) {
-//         console.log('hi');
-//         //return img (value of that key that matches)
-//     }
-// }
-
-
 //make an object with key:value pairs
 //loop through the key pairs then return the value (img) 
-//graph that img to the const flashcard alphabet 
-
-
-isButtonClicked = false
-letter_element.textContent = generate_random_letter();
+//graph that img to the const flashcard alphabet
 
 
 
+const generate_letter = document.getElementById('flashcard'); 
+const generate_button = document.getElementById('generate_button');
+const generate_alphabet_button = document.getElementById('generate_alphabet');
+// const letter_element = generate_letter.querySelector('.updated_letter');
+const previous_button = document.getElementById('prev_button')
+// Initialize the current flashcard value
+let current_flashcard = generate_random_letter();
+const letter_element = document.querySelector('.updated_letter');
 
-function alphabet_button() {
-    letter_element.textContent = generate_alphabet();
-    isButtonClicked = true
-    return letter_element.textContent;
-}
+// Set the initial flashcard value
+letter_element.textContent = current_flashcard;
 
-generate_alphabet_button.addEventListener('click', alphabet_button);
-
-function random_letter_button() {
-    letter_element.textContent = generate_random_letter();
-    isButtonClicked = true;
-    return letter_element.textContent;
-}
-
-generate_button.addEventListener('click', random_letter_button);
-
-// console.log(random_letter_button())
-
-function hello() {
-    if (alphabet_button() === 'B') {
-        console.log('hello')
+let previous_flashcard_list = []
+let index = 1
+// Event listener for the previous flashcard button
+previous_button.addEventListener('click', function () {
+    console.log(previous_flashcard_list)
+    if (index === previous_flashcard_list.length) {
+        letter_element.textContent = previous_flashcard_list[0];
     }
-}
+    else if (index < previous_flashcard_list.length) {
+        index = 1;
+    }
+    else {
+        letter_element.textContent = previous_flashcard_list[previous_flashcard_list.length - index];
+        index += 1;
+    }
+    
+});
+
+// if the list reaches the end don't print out anymore and empty the list
 
 
-const flashcard_alphabet = document.getElementById('flashcard_alphabet');
+// Event listener for the alphabet button
+generate_alphabet_button.addEventListener('click', function () {
+    previous_flashcard = current_flashcard;
+    previous_flashcard_list.push(previous_flashcard)
+    current_flashcard = generate_alphabet();
+    letter_element.textContent = current_flashcard;
+});
 
-flashcard_alphabet.addEventListener('click', function () {
+// Event listener for the next flashcard button
+generate_button.addEventListener('click', function () {
+    previous_flashcard = current_flashcard;
+    previous_flashcard_list.push(previous_flashcard)
+    current_flashcard = generate_random_letter();
+    letter_element.textContent = current_flashcard;
+});
+
+
+
+
+
+
+// letter_element.textContent = generate_random_letter();
+
+
+
+// generate_alphabet_button.addEventListener('click', function() {
+//     letter_element.textContent = generate_alphabet()
+// });
+
+// generate_button.addEventListener('click', function() {
+//     letter_element.textContent = generate_random_letter();
+// });
+
+
+// change the flashcard on click 
+const flashcard = document.getElementById('flashcard');
+
+flashcard.addEventListener('click', function () {
     this.classList.toggle('opaque');
 });
