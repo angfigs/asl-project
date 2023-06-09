98a11837ba536ecb074d622c8b932b8d558bb5c0
angfigs <angfigs1475@gmail.com>
2023-06-08 21:48:46 -0700

    Fixed image toggle and previous flashcard button.

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 const alphabet_object = {
-    A: './images/a.png',
-    B: 'b',
-    C: 'c',
-    D: 'd',
-    E: 'e',
-    F: 'f',
-    G: 'g',
-    H: 'h',
-    I: 'i',
-    J: 'j',
-    K: 'k',
-    L: 'l',
-    M: 'm',
-    N: 'n',
-    O: 'o',
-    P: 'p',
-    Q: 'q',
-    R: 'r',
-    S: 's',
-    T: 't',
-    U: 'u',
-    V: 'v',
-    W: 'w',
-    X: 'x',
-    Y: 'y',
-    Z: 'z'
+    A: './images/alphabet/a.png',
+    B: './images/alphabet/b.png',
+    C: './images/alphabet/c.png',
+    D: './images/alphabet/d.png',
+    E: './images/alphabet/e.png',
+    F: './images/alphabet/f.png',
+    G: './images/alphabet/g.png',
+    H: './images/alphabet/h.png',
+    I: './images/alphabet/i.png',
+    J: './images/alphabet/j.gif',
+    K: './images/alphabet/k.png',
+    L: './images/alphabet/l.png',
+    M: './images/alphabet/m.png',
+    N: './images/alphabet/n.png',
+    O: './images/alphabet/o.png',
+    P: './images/alphabet/p.png',
+    Q: './images/alphabet/q.png',
+    R: './images/alphabet/r.png',
+    S: './images/alphabet/s.png',
+    T: './images/alphabet/t.png',
+    U: './images/alphabet/u.png',
+    V: './images/alphabet/v.png',
+    W: './images/alphabet/w.png',
+    X: './images/alphabet/x.png',
+    Y: './images/alphabet/y.png',
+    Z: './images/alphabet/z.gif',
+    '': './images/alphabet/dance.gif',
 };
 
 function create_alphabet(alphabet_obj) {
@@ -64,22 +65,69 @@ const letter_element = document.querySelector('.updated_letter');
 let current_flashcard = generate_random_letter();
 letter_element.textContent = current_flashcard;
 
+// Change the flashcard on click 
+const flashcard = document.getElementById('flashcard');
+
+function add_image_element(image_path) {
+    const image = document.createElement('img');
+    image.src = image_path;
+    image.style.width = '750px';
+    image.style.height = '550px';
+    image.style.opacity = '0'; 
+    image.style.transition = 'opacity 0.5s ease-in-out';
+    flashcard.appendChild(image);
+    setTimeout(() => {
+        image.style.opacity = '1';
+    }, 10);
+}
+
+
+//Check if an image element already exists and remove it
+function remove_image() {
+    const existingImage = flashcard.querySelector('img');
+    if (existingImage) {
+        flashcard.removeChild(existingImage);
+    }
+}
+
+img_on = true
+function display_image() {
+    if (img_on === true) {
+        letter_element.textContent = '';
+        const image_path = alphabet_object[current_flashcard];
+        remove_image();
+        add_image_element(image_path);
+        img_on = false;
+    } else {
+        remove_image();
+        letter_element.textContent = current_flashcard;
+        img_on = true;
+    }
+}
+
+flashcard.addEventListener('click', function () {
+    this.classList.toggle('opaque');
+    display_image();
+});
 
 let previous_flashcard_list = []
 let index = 1
 // Event listener for the previous flashcard button
 previous_button.addEventListener('click', function () {
     if (previous_flashcard_list.length === 0) {
-        letter_element.textContent = '';
+        current_flashcard = '';
+        letter_element.textContent = current_flashcard;
     }
     else if (index === previous_flashcard_list.length) {
-        letter_element.textContent = previous_flashcard_list[0];
+        current_flashcard = previous_flashcard_list[0];
+        letter_element.textContent = current_flashcard
         index = 1;
         previous_flashcard_list = [];
     }
     else if (index >= 1 && previous_flashcard_list.length != 0) {
-        letter_element.textContent = previous_flashcard_list[previous_flashcard_list.length - index];
-        index += 1;
+        current_flashcard = previous_flashcard_list[previous_flashcard_list.length - index];
+        letter_element.textContent = current_flashcard;
+        index++;
     }
 });
 
@@ -92,7 +140,7 @@ generate_alphabet_button.addEventListener('click', function () {
 });
 
 // Event listener for the next/shuffle cards button
-const shuffle_cards = function () {
+function shuffle_cards() {
     previous_flashcard = current_flashcard;
     previous_flashcard_list.push(previous_flashcard)
     current_flashcard = generate_random_letter();
@@ -101,37 +149,3 @@ const shuffle_cards = function () {
 
 generate_button.addEventListener('click', shuffle_cards);
 shuffle_cards_button.addEventListener('click', shuffle_cards);
-
-// Change the flashcard on click 
-const flashcard = document.getElementById('flashcard');
-
-function add_image(image_path) {
-    console.log(image_path)
-    const image = document.createElement('img');
-    image.src = image_path;
-
-    // Apply custom styles to the image
-    image.style.width = '750px';
-    image.style.height = '550px';
-
-    flashcard.appendChild(image);
-}
-
-function display_image() {
-    const current_value = letter_element.textContent;
-    if (current_value === current_flashcard) {
-        letter_element.textContent = '';
-        const image_path = alphabet_object[current_value];
-        console.log(image_path)
-        add_image(image_path);
-    } else {
-        console.log(current_value);
-        flashcard.removeChild(flashcard.children[1]);
-        letter_element.textContent = current_flashcard;
-    }
-}
-
-flashcard.addEventListener('click', function () {
-    this.classList.toggle('opaque');
-    display_image();
-});
